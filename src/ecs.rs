b209392a54695e0b74395f4d312d6fb9d1eafa8b
ProgramCrafter <pythonhunter146@gmail.com>
2024-12-31 04:38:49 +0300

    Slider, without tooltips yet

--- src/ecs.rs
+++ src/ecs.rs
@@ -17,10 +17,12 @@ pub fn expense_category_slider(ui: &mut Ui, pos: &mut f32, held: &mut usize,
     let n = options.len();
     let nf = n as f32;
     
-    let mark_diam = ui.spacing().interact_size.y / 3.0;
+    let mark_diam = ui.spacing().interact_size.y / 2.0;
     let slot_diam = mark_diam * 1.9;
     
     let w = ui.available_width();
+//----------------------------------------------------------------------------//
+    // SECTION 1: Slider.
     let sl_size = vec2(w, mark_diam);
     let (slider_rect, mut re) = ui.allocate_exact_size(sl_size,
         Sense::click_and_drag());
@@ -43,22 +45,52 @@ pub fn expense_category_slider(ui: &mut Ui, pos: &mut f32, held: &mut usize,
     let (first_pos, second_pos) = (point_at(0.0), point_at(nf - 1.0));
     ui.painter().line(vec![first_pos, second_pos], (mark_diam, WHITE));
     
-    if *held == *pos as usize {
+//----------------------------------------------------------------------------//
+    // SECTION 1.5: Moving parts of slider.
+    if re.drag_stopped() {
+        *pos = *held as f32;  // need to snap
+    } else if re.clicked() {
+        if let Some(p) = re.ctx.input(|i| i.pointer.latest_pos()) {
+            *pos = remap_clamp(p.x, slider_rect.x_range(), 0.0..=(nf-1.0))
+                .round();
+            let want = *pos as usize;
+            if want != *held {
+                *held = want; re.mark_changed();
+            }
+        }
+    } else if re.is_pointer_button_down_on() {
+        if let Some(p) = re.ctx.input(|i| i.pointer.latest_pos()) {
+            *pos = remap_clamp(p.x, slider_rect.x_range(), 0.0..=(nf-1.0));
+            let mut want = *held;
+            
+            if pos.fract() < 0.12 {
+                *pos = pos.floor(); want = *pos as usize;
+            } else if pos.fract() > 1. - 0.12 {
+                *pos = pos.ceil();  want = *pos as usize;
+            }
+            
+            if want != *held {
+                *held = want; re.mark_changed();
+            }
+        }
+    }
+    
+    if pos.fract() == 0.0 {
         // Drawing colored point.
         let pos = point_at(*pos);
-//----------------------------------------------------------------------------//
         ui.painter().circle(pos, mark_diam / 2.0, options[*held].1, NO_STROKE);
     } else {
         let pos_held = point_at(*held as f32);
         let pos_move = point_at(*pos);
         
+        // Drawing slightly-colored and gray points.
         ui.painter().circle(
             pos_held, mark_diam / 2.0,
             options[*held].1.gamma_multiply(0.2), NO_STROKE
         );
         ui.painter().circle(
             pos_move, mark_diam / 2.0,
-            Color32::LIGHT_GRAY, NO_STROKE
+            Color32::LIGHT_GRAY, (1.0, Color32::DARK_GRAY)
         );
     }
     
